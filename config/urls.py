cb3ed1d1ec854b3e0045a83291cd29d60270ead8
suhyun <id_suhyun@naver.com>
2024-08-22 15:00:04 +0900

    ✨feat: 1일차 과제 수정(브랜치 테스트)

--- config/urls.py
+++ config/urls.py
@@ -9,19 +9,19 @@ _db = user_db
 
 
 def user_list(request):
-    names = [{'id': key, 'name': value['이름']} for key, value in _db.items()]
-    return render(request, 'user_list.html', {'data': names})
+    names = [{"id": key, "name": value["이름"]} for key, value in _db.items()]
+    return render(request, "user_list.html", {"data": names})
 
 
 def user_info(request, user_id):
     if user_id > len(_db):
-        raise Http404('User not found')
+        raise Http404("User not found")
     info = _db[user_id]
-    return render(request, 'user_info.html', {'data': info})
+    return render(request, "user_info.html", {"data": info})
 
 
 urlpatterns = [
-    path('users/', user_list, name='user_list'),
-    path('users/<int:user_id>/', user_info, name='user_info'),
-    path('admin/', admin.site.urls),
+    path("users/", user_list, name="user_list"),
+    path("users/<int:user_id>/", user_info, name="user_info"),
+    path("admin/", admin.site.urls),
 ]
